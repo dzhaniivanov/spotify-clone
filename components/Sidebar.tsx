d6bcb66baa3778d1f1764738f6f578fc6cefd603
dzhaniivanov94 <68897146+dzhaniivanov94@users.noreply.github.com>
2023-06-15 17:34:17 +0300

    mediaItem component added

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,12 +6,14 @@ import { BiSearch } from "react-icons/bi";
 import Box from "./Box";
 import SidebarItem from "./SidebarItem";
 import Library from "./Library";
+import { Song } from "@/types";
 
 interface SidebarProps {
   children: React.ReactNode;
+  songs: Song[];
 }
 
-const Sidebar: React.FC<SidebarProps> = ({ children }) => {
+const Sidebar: React.FC<SidebarProps> = ({ children, songs }) => {
   const pathname = usePathname();
 
   const routes = useMemo(
@@ -20,14 +22,14 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
         icon: HiHome,
         label: "Home",
         active: pathname !== "/search",
-        href: "/",
+        href: "/"
       },
       {
         icon: BiSearch,
         label: "Search",
         active: pathname === "/search",
-        href: "/search",
-      },
+        href: "/search"
+      }
     ],
     [pathname]
   );
@@ -36,13 +38,13 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
       <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2">
         <Box>
           <div className="flex flex-col gap-y-4 px-5 py-4">
-            {routes.map((item) => (
+            {routes.map(item => (
               <SidebarItem key={item.label} {...item} />
             ))}
           </div>
         </Box>
         <Box className="overflow-y-auto h-full">
-          <Library />
+          <Library songs={songs} />
         </Box>
       </div>
       <main className="h=full flex-1 overflow-y-auto py-2">{children}</main>
