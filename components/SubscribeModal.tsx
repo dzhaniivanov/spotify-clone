47a71c583799ebbf98f0bbaa3a223ea48be0c32e
dzhaniivanov94 <68897146+dzhaniivanov94@users.noreply.github.com>
2023-06-21 10:47:25 +0300

    work on stripe webhook,susbcrribe modal

---
+++ components/SubscribeModal.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import Modal from "./Modal";
+
+const SubscribeModal = () => {
+  return (
+    <Modal
+      title="Only for premium users"
+      description="Listen to music with Spotify Premium"
+      isOpen
+      onChange={() => {}}
+    >
+      subscription
+    </Modal>
+  );
+};
+export default SubscribeModal;
--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 
 import AuthModal from "@/components/AuthModal";
 import UploadModal from "@/components/UploadModal";
+import SubscribeModal from "@/components/SubscribeModal";
 
 const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -20,6 +21,7 @@ const ModalProvider = () => {
     <>
       <AuthModal />
       <UploadModal />
+      <SubscribeModal />
     </>
   );
 };
